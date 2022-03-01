de30db02387237c1bd334b0dd9676b734f1cbb15
Joakim Olsson <joakim.olsson@adicitus.cloud>
2022-03-01 11:48:20 +0100

    Added support for query parameters.
    + Added query parameters when retrieving single meetings.

--- source/Meetings/Get-ZoomAPIMeeting.ps1
+++ source/Meetings/Get-ZoomAPIMeeting.ps1
@@ -3,27 +3,31 @@ function Get-ZoomAPIMeeting {
     param(
         [Parameter(Mandatory=$true)]
         [PSCustomObject]$Token,
-        [Parameter(Mandatory=$true, ParameterSetName="HostedMeetings")]
-        [String]$UserId,
-        [Parameter(Mandatory=$true, ParameterSetName="MeetingID")]
+        [Parameter(Mandatory=$true, ParameterSetName="SingleMeeting")]
         [String]$MeetingId,
-        [Parameter(Mandatory=$false, ParameterSetName="HostedMeetings", HelpMessage="Type of meeting to retrieve.")]
+        [Parameter(Mandatory=$false, ParameterSetName="SingleMeeting", HelpMessage="Specify to retrieve a specific occurrence.")]
+        [string]$OccurrenceId,
+        [Parameter(Mandatory=$false, ParameterSetName="SingleMeeting", HelpMessage="Setting this to true will return previous occurrences of this meeting as well as future ones.")]
+        [bool]$ShowPastOccurrences,
+        [Parameter(Mandatory=$true, ParameterSetName="ListMeetings", HelpMessage="ID of the user for which to retrieve meetings.")]
+        [String]$UserId,
+        [Parameter(Mandatory=$false, ParameterSetName="ListMeetings", HelpMessage="Type of meeting to retrieve.")]
         [ValidateSet('scheduled', 'upcomig', 'live')]
         [String]$Type,
-        [Parameter(Mandatory=$false, ParameterSetName="HostedMeetings", HelpMessage="Number of meetings to return for each request/page (max 300).")]
+        [Parameter(Mandatory=$false, ParameterSetName="ListMeetings", HelpMessage="Number of meetings to return for each request/page (max 300).")]
         [ValidateRange(1, 300)]
         [int]$PageSize,
-        [Parameter(Mandatory=$false, ParameterSetName="HostedMeetings", HelpMessage="The page number of the page to return.")]
+        [Parameter(Mandatory=$false, ParameterSetName="ListMeetings", HelpMessage="The page number of the page to return.")]
         [ValidateRange(1, 2147483647)]
         [int]$PageNumber,
-        [Parameter(Mandatory=$false, ParameterSetName="HostedMeetings", HelpMessage='The next page token is used to paginate through large result sets. A next page token will be returned whenever the set of available results exceeds the current page size. The expiration period for this token is 15 minutes.')]
+        [Parameter(Mandatory=$false, ParameterSetName="ListMeetings", HelpMessage='The next page token is used to paginate through large result sets. A next page token will be returned whenever the set of available results exceeds the current page size. The expiration period for this token is 15 minutes.')]
         [string]$NextPageToken
     )
     
     $endpoint = $null
 
     switch ($PSCmdlet.ParameterSetName) {
-        "HostedMeetings" {
+        "ListMeetings" {
             $endpoint = "users/{0}/meetings" -f $UserId
 
             $options = @{
@@ -35,9 +39,15 @@ function Get-ZoomAPIMeeting {
 
             Invoke-ZoomAPIRequest -Method Get -Endpoint $Endpoint -Token $Token -QueryParamMap $options -QueryParamSrc $PSBoundParameters
         }
-        "MeetingID" {
+        "SingleMeeting" {
             $endpoint = "meetings/{0}" -f $MeetingId
-            Invoke-ZoomAPIRequest -Method Get -Endpoint $Endpoint -Token $Token
+
+            $options = @{
+                'OccurrenceId' = { param($v) 'occurrence_id={0}' -f $v }
+                'ShowPastOccurrences' = { param($v) 'show_previous_occurrences={0}' -f $v.toString().toLower() }
+            }
+
+            Invoke-ZoomAPIRequest -Method Get -Endpoint $Endpoint -Token $Token -QueryParamMap $options -QueryParamSrc $PSBoundParameters
         }
     }
 
